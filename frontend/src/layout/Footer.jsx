ff89e4c7e9c1881ffcf4a28441bcf16fa71b0bae
lake_hu <huhengshuai@xiaomi.com>
2023-03-26 21:50:53 +0800

    add contact

--- frontend/src/layout/Footer.jsx
+++ frontend/src/layout/Footer.jsx
@@ -1,5 +1,10 @@
-export default function(){
-    return <footer className="h-[86px] bg-white text-[#5D5D5B] flex items-center justify-center shadow-[0px_-2px_4px_rgba(0, 0, 0, 0.05)]">
-        © Copyright 2023 TBOOK PTE LTD. All rights reserved
+export default function () {
+  return (
+    <footer className="h-[86px] bg-white text-[#5D5D5B] flex items-center justify-center shadow-[0px_-2px_4px_rgba(0, 0, 0, 0.05)]">
+      <p>© A scaling ethereum hackthon product by TBOOK team</p>
+      <a href="https://t.me/Peter_TBook" target="_blank" className="ml-10 text-base font-bold text-black">
+        Contact Me
+      </a>
     </footer>
+  );
 }
